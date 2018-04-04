31acaa334ec25afeb08f210579bd96255325a6fa
Zanghi <nicholas.zanghi@optum.com>
2018-04-04 00:24:26 -0400

    implemented nav menu for smaller screen, need to make it display actual nav menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,60 +1,73 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Media from 'react-media';
 
 const active = {
   'text-decoration': 'underline'
 };
 
-const Nav = () => {
-  return (
-    <ul className="nav" role="navigation">
-      <li
-        onClick={() => {
-          document.getElementById('main').scrollIntoView({ behavior: 'smooth' });
-        }}
-      >
-        <NavLink to="/" activeClassName="active" exact={true}>
-          Main
-        </NavLink>
-      </li>
-      <li
-        onClick={() => {
-          document.getElementById('main').scrollIntoView({ behavior: 'smooth' });
-        }}
-      >
-        <NavLink to="/services" activeClassName="active">
-          Services
-        </NavLink>
-      </li>
-      <li
-        onClick={() => {
-          document.getElementById('main').scrollIntoView({ behavior: 'smooth' });
-        }}
-      >
-        <NavLink to="/photonic-light-therapy" activeClassName="active">
-          Photonic Light Therapy
-        </NavLink>
-      </li>
-      <li
-        onClick={() => {
-          document.getElementById('main').scrollIntoView({ behavior: 'smooth' });
-        }}
-      >
-        <NavLink to="/sport-massage" activeClassName="active">
-          Sport Massage
-        </NavLink>
-      </li>
-      <li
-        onClick={() => {
-          document.getElementById('main').scrollIntoView({ behavior: 'smooth' });
-        }}
-      >
-        <NavLink to="/testimonials" activeClassName="active">
-          Testimonials
-        </NavLink>
-      </li>
-    </ul>
-  );
-};
+class Nav extends React.Component {
+  render() {
+    return (
+      <Media query={{ maxWidth: 990 }}>
+        {matches =>
+          matches ? (
+            <button className="hamburger" onClick={() => console.log('have to toggle overlay')}>
+              &#8801;
+            </button>
+          ) : (
+            <ul className="nav" role="navigation">
+              <li
+                onClick={() => {
+                  window.scroll({ top: 750, left: 0, behavior: 'smooth' });
+                }}
+              >
+                <NavLink to="/" activeClassName="active" exact={true}>
+                  Main
+                </NavLink>
+              </li>
+              <li
+                onClick={() => {
+                  window.scroll({ top: 750, left: 0, behavior: 'smooth' });
+                }}
+              >
+                <NavLink to="/services" activeClassName="active">
+                  Services
+                </NavLink>
+              </li>
+              <li
+                onClick={() => {
+                  window.scroll({ top: 750, left: 0, behavior: 'smooth' });
+                }}
+              >
+                <NavLink to="/photonic-light-therapy" activeClassName="active">
+                  Photonic Light Therapy
+                </NavLink>
+              </li>
+              <li
+                onClick={() => {
+                  window.scroll({ top: 750, left: 0, behavior: 'smooth' });
+                }}
+              >
+                <NavLink to="/sport-massage" activeClassName="active">
+                  Sport Massage
+                </NavLink>
+              </li>
+              <li
+                onClick={() => {
+                  window.scroll({ top: 750, left: 0, behavior: 'smooth' });
+                }}
+              >
+                <NavLink to="/testimonials" activeClassName="active">
+                  Testimonials
+                </NavLink>
+              </li>
+            </ul>
+          )
+        }
+      </Media>
+    );
+  }
+}
 
 export default Nav;
