6fff1218a98154b39ad89eecbf6f99a7c1baf08d
nzanghi <nzanghi@github.com>
2018-04-13 01:54:27 -0400

    fixed broken social media icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { SocialIcon } from 'react-social-icons';
 
+import FooterLogo from '../assets/images/Logo.png';
+
 const Footer = () => {
   return (
     <div className="footer">
-        <div id="social-media">
-          <SocialIcon className="social-media-item" url="mailto:nicole.hahn07@gmail.com" />
-          <SocialIcon className="social-media-item" url="https://www.facebook.com/TherapeticHands/" />
-          <SocialIcon className="social-media-item" url="https://www.instagram.com/therapetichands/" />
+    <img id="footer-logo" src={FooterLogo}></img>
+        <div id="fun">
+          <SocialIcon className="fun-item" url="mailto:nicole.hahn07@gmail.com" />
+          <SocialIcon className="fun-item" url="https://www.facebook.com/TherapeticHands/" />
+          <SocialIcon className="fun-item" url="https://www.instagram.com/therapetichands/" />
         </div>
         <p id="copyright">© Therapetic Hands | Canine and Equine Massage</p>
     </div>
