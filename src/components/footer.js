2911bf07fa9790c01c78308fbb082c41efce857e
Zanghi <nicholas.zanghi@optum.com>
2018-04-18 22:19:48 -0400

    messing with fixed footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,23 +8,28 @@ class Footer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      innderWidth: window.innerWidth
+      footerOffset: null
     };
+    this.handleResize = window.addEventListener('resize', this.handleResize.bind(this));
   }
   componentDidMount() {
-    window.addEventListener('resize', () => {
-      const innerWidth = window.innerWidth;
-      this.setState({ innerWidth });
+    this.setState({
+      footerOffset: this.refs.Footer.offsetTop - this.refs.Footer.offsetHeight
+    });
+  }
+  handleResize() {
+    this.setState({
+      footerOffset: this.refs.Footer.offsetTop - this.refs.Footer.offsetHeight
     });
   }
   render() {
-    const check = this.props.scrollY > 100;
+    const check = this.props.scrollY > this.state.footerOffset;
     return (
       <div className="footer" ref="Footer">
         <div className="footer-logo-container">
           <img id="footer-logo" src={FooterLogo} />
         </div>
-        <div className={check ? 'fun snap' : 'fun'}>
+        <div className={check ? 'social-container snap' : 'social-container'}>
           <SocialIcon className={check ? 'fun-item locked' : 'fun-item'} url="mailto:nicole.hahn07@gmail.com" />
           <SocialIcon className={check ? 'fun-item locked' : 'fun-item'} url="https://www.facebook.com/TherapeticHands/" />
           <SocialIcon className={check ? 'fun-item locked' : 'fun-item'} url="https://www.instagram.com/therapetichands/" />
