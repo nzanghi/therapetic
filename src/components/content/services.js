8aa474f1997941a9b4dc8b904c12a57100961bd0
Zanghi <nicholas.zanghi@optum.com>
2018-04-23 23:30:57 -0400

    implemented minmax for better content resizing. working on layout design

--- src/components/content/services.js
+++ src/components/content/services.js
@@ -1,33 +1,42 @@
 import React from 'react';
 
+const top = require('../../assets/images/massage-pics/main-horse-massage-top.JPG');
+const bottom = require('../../assets/images/massage-pics/cute.JPG');
+
 const Services = () => {
   return (
-    <div className="main">
-      <section className="main-container">
-        <h1>Services</h1>
-        <div className="main-content">
-          <p className="main-content-text">
-            Nicole has always had a love for Animals and decided at a young age to pursue her passion and promised to find a way to help any
-            animal at any cost. She is certified in Equine and Canine Sports Massage therapy, and also offers exciting Photonic Light
-            Therapy.
-          </p>
-          <p className="main-content-text">
-            Nicole is a very experienced animal handler and has worked as a dog care specialist for many years. She has worked with horses
-            from a very young age, owning horses, showing and training horses, teaching novice horse enthusiasts how to properly handle
-            horses, and being a stable hand for 6 years.
-          </p>
+    <section className="main">
+      <h1 id="main-header">Services</h1>
+      <div className="main-content">
+        <div className="content-block">
+          <img className="main-image-top" src={top} />
+          <div className="text-block">
+            <h3>Horse:</h3>
+            <p className="text-block-text">Full body massage and light treatment: 80</p>
+            <p className="text-block-text">Just massage: 70</p>
+            <p className="text-block-text">Just light (full body): 60</p>
+            <p className="text-block-text">Specific area for lights: 30-50</p>
+          </div>
         </div>
-        <div className="main-content">
-          <p className="main-content-text">
-            She attended Rutgers University where she participated in the program Rutgers University Teaching Herd where she promoted horses
-            that needed to be adopted and taught them proper ground manners.
-          </p>
-          <p className="main-content-text">
-            Nicole has a true passion for what she does, and you and your animals will most certainly experience this in every session.
-          </p>
+        <div className="content-block">
+          <div className="text-block">
+            <h3>Dog: pricing depends on size</h3>
+            <p className="text-block-text">Full body massage and light: 40-60</p>
+            <p className="text-block-text">Just massage: 35-50</p>
+            <p className="text-block-text">Just light 30-40</p>
+            <p className="text-block-text">Specific are: 25-400</p>
+          </div>
+          <img className="main-image-bottom" src={bottom} />
         </div>
-      </section>
-    </div>
+        <div className="content-block">
+          <div className="text-block">
+            <p className="text-block-text">Note: Length of massage depends on the size of the animal</p>
+            <p className="text-block-text">Horse massage will last anywhere between 45 minutes and an hour</p>
+            <p className="text-block-text">Dog massage will last anywhere between 20 and 45 minutes</p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
